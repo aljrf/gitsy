e96d023b3029660d00bab9bf6852a549f9ffccf8
aljrf <javier.rodriguez.frias@gmail.com>
2025-06-15 19:13:34 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Gitsy",
+    pageTitle: "Gutsy",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
